021ab841b38a33df23c88b91cc854ffa079e8e1e
Jared C. Gober <jcgober@Jareds-Air.lan1>
2020-09-28 18:14:47 -0400

    Added grid.js and grid.css

---
+++ src/grid.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './grid.css';
+
+
+function Grid() {
+    return (
+    <div className="App">
+
+<ul className="list-group list-group-horizontal">
+  <li className="list-group-item">Cras justo odio</li>
+  <li className="list-group-item">Dapibus ac facilisis in</li>
+  <li className="list-group-item">Morbi leo risus</li>
+</ul>
+
+<ul className="list-group list-group-horizontal">
+  <li className="list-group-item">Cras justo odio</li>
+  <li className="list-group-item">Dapibus ac facilisis in</li>
+  <li className="list-group-item">Morbi leo risus</li>
+</ul>
+
+<ul className="list-group list-group-horizontal">
+  <li className="list-group-item">Cras justo odio</li>
+  <li className="list-group-item">Dapibus ac facilisis in</li>
+  <li className="list-group-item">Morbi leo risus</li>
+</ul>
+
+
+
+    </div>
+    );
+  }
+
+  export default Grid;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './grid.css';
 import Navbar from './navbar'
+import Grid from './grid'
+
 
 
 import * as serviceWorker from './serviceWorker';
@@ -9,6 +12,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+
+    <Grid />
 
   </React.StrictMode>,
   document.getElementById('root')
