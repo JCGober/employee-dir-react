da2af0c1db0f69a6931dfea28bd5ffffb388cda6
Jared C. Gober <jcgober@Jareds-Air.lan1>
2020-09-28 18:46:18 -0400

    updates to employee grid

--- src/grid.js
+++ src/grid.js
@@ -6,23 +6,52 @@ function Grid() {
     return (
     <div className="App">
 
-<ul className="list-group list-group-horizontal">
-  <li className="list-group-item">Cras justo odio</li>
-  <li className="list-group-item">Dapibus ac facilisis in</li>
-  <li className="list-group-item">Morbi leo risus</li>
-</ul>
-
-<ul className="list-group list-group-horizontal">
-  <li className="list-group-item">Cras justo odio</li>
-  <li className="list-group-item">Dapibus ac facilisis in</li>
-  <li className="list-group-item">Morbi leo risus</li>
-</ul>
-
-<ul className="list-group list-group-horizontal">
-  <li className="list-group-item">Cras justo odio</li>
-  <li className="list-group-item">Dapibus ac facilisis in</li>
-  <li className="list-group-item">Morbi leo risus</li>
-</ul>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Employee Id#</th>
+      <th scope="col">Position</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Email</th>
+      <th scope="col">Salary</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">7193</th>
+      <td>Web Development Team</td>
+      <td>Jared</td>
+      <td>Gober</td>
+      <td>jaredcgober@gmail.com</td>
+      <td>$75,000</td>
+    </tr>
+    <tr>
+      <th scope="row">7194</th>
+      <td>Ball Boy</td>
+      <td>Jacob</td>
+      <td>Gober</td>
+      <td>jacobGober@example.net</td>
+      <td>$11,000</td>
+    </tr>
+    <tr>
+      <th scope="row">0014</th>
+      <td>Player</td>
+      <td>Pierre-Emmerick</td>
+      <td>Aubameyang</td>
+      <td>IScoreGoalz@example.net</td>
+      <td>$350,000</td>
+    </tr>
+    <tr>
+      <th scope="row">3272</th>
+      <td>Acountant</td>
+      <td>Rebecca</td>
+      <td>Johnson</td>
+      <td>ArseanlAccounts@example.net</td>
+      <td>$120,000</td>
+    </tr>
+  </tbody>
+</table>
 
 
 
