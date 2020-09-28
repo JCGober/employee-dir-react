8b9c1abd048c59ce9b207289bc26f330b44d6c09
Jared C. Gober <jcgober@Jareds-Air.lan1>
2020-09-28 17:56:30 -0400

    Created Navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from './navbar'
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Navbar() {
+    return (
+    <div className="App">
+
+    <nav className="navbar navbar-light bg-light">
+    <span className="navbar-brand mb-0 h1">Employee Directory</span>
+    </nav>
+
+
+    </div>
+    );
+  }
+
+  export default Navbar;
